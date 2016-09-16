048fbf738e480620ef7ee0ab41d3582344b58929
Radu Patulescu <radu.patulescu@gmail.com>
2016-09-16 17:55:54 +0100

    Initial commit

---
+++ matchthis/imports/ui/body.js
@@ -0,0 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import { Items } from '../api/items.js';
+
+import './body.html';
+
+Template.body.helpers({
+    items() {
+    return Items.find({});
+  },
+});
